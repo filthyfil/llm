814b435840c672e839cff2ed74ce1b3d43eaf99d
filthyfil <filjasionek@gmail.com>
2025-06-28 16:44:26 -0400

    model

---
+++ model/model.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from model.transformer_block import TransformerBlock, LayerNorm
+
+
+# GPTModel is a simple implementation of the GPT architecture
+# It consists of an embedding layer, multiple transformer blocks, and a final output head.
+# The embedding layer converts input token indices into dense vectors,
+# the transformer blocks process these embeddings through self-attention and feed-forward networks,
+# and the output head produces logits for each token in the vocabulary.    
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_embed = nn.Embedding(cfg["vocab_size"], cfg["dim_embed"])
+        self.pos_embed = nn.Embedding(cfg["context_length"], cfg["dim_embed"])  
+        self.drop_embed = nn.Dropout(cfg["drop_rate"])
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+
+        self.final_norm = LayerNorm(cfg["dim_embed"])
+        self.out_head = nn.Linear(
+            cfg["dim_embed"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embed = self.tok_embed(in_idx)
+        pos_embed = self.pos_embed(torch.arange(seq_len, device=in_idx.device))
+        
+        x = tok_embed + pos_embed
+        x = self.drop_embed(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+
+        logits = self.out_head(x)
+        return logits
